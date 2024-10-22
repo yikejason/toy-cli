371b60b5632b10d6567511b7cd913bf2f41d3414
yikejason <18215513386@163.com>
2024-10-22 14:57:35 +0800

    feature: support csv command with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use toy_cli::{process_csv, Opts, SubCommand};
+
+fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+        }
+    }
+
+    Ok(())
 }
---
+++ src/process.rs
@@ -0,0 +1,32 @@
+use std::fs;
+
+use anyhow::Result;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Player {
+    #[serde(rename = "Name")]
+    name: String,
+    #[serde(rename = "Position")]
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    #[serde(rename = "Nationality")]
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit: String,
+}
+pub fn process_csv(input: &str, output: &str) -> Result<()> {
+    let mut reader = Reader::from_path(input)?;
+    let mut ret = Vec::with_capacity(128);
+    for result in reader.deserialize() {
+        let record: Player = result?;
+        ret.push(record);
+    }
+
+    let json = serde_json::to_string_pretty(&ret)?;
+    fs::write(output, json)?;
+
+    Ok(())
+}
